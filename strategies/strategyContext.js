c9763e1dbf12748cbc16aa9be23aca54c7809f08
luiscelismx <3344810+luiscelismx@users.noreply.github.com>
2022-02-01 12:27:01 -0600

    Create strategyContext.js

---
+++ strategies/strategyContext.js
@@ -0,0 +1,20 @@
+/**
+ * It creates an array of strategies based on the context/data, the
+ * context will determine which strategies are applicable.
+ *
+ * It looks for specific update events to partially update a document (order)
+ * default will do a full update for complex updates.
+ */
+export default class StrategyContext {
+  constructor() {
+    this.strategy = null;
+  }
+
+  execute() {
+    return this.strategy.getParams();
+  }
+
+  setStrategy(strategy) {
+    this.strategy = strategy;
+  }
+}
